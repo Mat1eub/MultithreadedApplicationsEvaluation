d0d997553022cb3c81d02f38186df6b85eeb65f0
Mateo Bauvir <bauvir.mateo@gmail.com>
2024-11-17 18:34:03 +0100

    Comments
    More comments about eat/think process on philosophers.c

--- src/philosophers.c
+++ src/philosophers.c
@@ -10,6 +10,10 @@ void* philosopher(void* param) {
     int left = (i + 1) % N;          
 
     for (size_t j = 0; j < COUNT; j++) {
+
+        // Philosophe pense
+        // On ne fait rien car penser est instantané
+
         if (i == N - 1) {            // Dernier philosophe prend les fourchettes dans un ordre inversé pour éviter un deadlock
             sem_wait(&forks[right]);
             sem_wait(&forks[left]);
@@ -19,7 +23,7 @@ void* philosopher(void* param) {
         }
 
         // Philosophe mange (section critique)
-        // Ici, on ne fait rien car manger est instantané.
+        // Ici aussi, on ne fait rien car manger est instantané.
 
         sem_post(&forks[left]);    // Le philosophe libère les fourchettes
         sem_post(&forks[right]);
