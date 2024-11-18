72baca42dd64c40e188fbbdbf88bf883ef441971
Mateo Bauvir <bauvir.mateo@gmail.com>
2024-11-19 00:58:58 +0100

    Prod/Cons headers with doc and test file

--- headers/producerconsummer.h
+++ headers/producerconsummer.h
@@ -1,6 +1,25 @@
 #ifndef PRODUCERCONSUMMER_H
 #define PRODUCERCONSUMMER_H
 
+/**
+ * @brief Fonction exécutée par les threads producteurs
+ * 
+ * Cette fonction produit un élément et l'ajoute au buffer partagé
+ * 
+ * @param arg Pointeur vers l'identifiant unique du thread
+ * @return NULL
+ */
 void* producer(void* arg);
+
+/**
+ * @brief Fonction exécutée par les threads consommateurs 
+ * 
+ * Lit un élément du buffer partagé
+ * 
+ * @param arg Argument passé au consommateur
+ * @return NULL
+ */
+void* consummer(void* arg);
+
 
 #endif //PRODUCERCONSUMMER_H
