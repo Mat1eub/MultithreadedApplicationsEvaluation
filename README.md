a04ce59f89d9a01d643596fd79f3abef94f31934
Mateo Bauvir <bauvir.mateo@gmail.com>
2024-11-20 17:15:33 +0100

    Finished commenting prod-cons problem
    TODO: Readme section & tests!

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Le problème du philosophe met en lumière les difficultés liés à la programm
 Un premier problème apparait assez rapidement en analysant la situation. Si tous les philosophes décident de d'abord prendre la fourchette du même côté de l'assiette alors chaque philosophe serait bloqué à attendre qu'un autre philosophe lâche une fourchette. En code, cette première version quelque peu naïve mènerait à un deadlock.
 
 Si le problème est que chaque philosphe doive prendre 2 ressources et le faire :
+
 - en 2 étapes,
 - tous dans le même ordre,
 - sans retour en arrière possible,
@@ -20,3 +21,17 @@ Il suffirait de supprimer une de ces conditions pour éviter un deadlock.
 Dans cette version, on a une situation binaire : un philosophe détient 0 ou 2 fourchettes mais jamais une seule. De plus, le dernier philosophe commence par prendre la fourchette de gauche tandis que les autres commencent par la fourchette de droite. Pourquoi cette deuxième condition ? Car il faut toujours garder en tête qu'un philosophe pourrait être trop lent et risquer de ne jamais avoir de fourchette disponible.
 
 ## Problème des producteurs-consommateurs
+
+
+// On met l'incrémentation dans le même mutex_prodcons,
+
+// une idée de faire autrement c'est de rajouter un mutex_prodcons pour incrémenter cette variable
+
+// (un thread incrémentera en même temps qu'un autre produira ca peut etre pas mal)
+
+
+// Parler dans le readme ou initialement que j'avais d'abord penser faire un while(1) if prodcount<10 : do else : break
+
+// sans penser qu'en fait plusieurs threads vont verifier cette condition et acceder au bloc dans le if et donc ca va produire
+
+// + que le seuil
--- src/producerconsummer.c
+++ src/producerconsummer.c
@@ -4,20 +4,16 @@
 #include <stdio.h>
 #include "../headers/producerconsummer.h"
 
-/**
- * Un mutex_prodcons pour bloquer l'accès au buffer 
- * Un sémaphore pour compter le nombre de cases du buffer remplies
- * Un sémaphore pour compter le nombre de cases du buffer vides
- */
+// Detailed explaination in the README's Producters-Consummers section
 
-// Variables initiales 
+// Initial variables
 #define N 8
+#define NB_ITEMS 131072
 int buff[N];
 int* buffer = buff;
 
-int prodcount = 0;      // Garde un compte du nombre de producteur
-int consumcount = 0;    // Garde un compte du nombre de consommateur
-
+int prodcount = 0;      // Keeps track of the number of producer
+int consumcount = 0;    // Keeps track of the number of consummer
 
 void process (){
     for (int i=0; i<10000; i++);
@@ -25,25 +21,22 @@ void process (){
 
 void* producer(void* arg){
     int* id = (int*)arg;
-    static int ip = 0;  // Pour le buffer
+    static int ip = 0;   // Indicates where we are in the buffer
     while(1){
         sem_wait(&cvides);
         pthread_mutex_lock(&mutex_prodcons);
 
-        if (prodcount>=131072) // 131072
+        if (prodcount>=NB_ITEMS)
         {
             pthread_mutex_unlock(&mutex_prodcons);
             break;
         }
         
-        // Section critique, écrire l'id du thread dans le buffer
-        // On met l'incrémentation dans le même mutex_prodcons,
-        // une idée de faire autrement c'est de rajouter un mutex_prodcons pour incrémenter cette variable
-        // (un thread incrémentera en même temps qu'un autre produira ca peut etre pas mal)
+        // Critical section
         prodcount++; 
         buffer[ip] = *id;
         ip = (ip+1)%N;
-        printf("Producer %d: prodcount=%d, ip=%d\n", *id, prodcount, ip);
+        // Debugging : printf("Producer %d: prodcount=%d, ip=%d\n", *id, prodcount, ip);
 
         pthread_mutex_unlock(&mutex_prodcons);
         process();
@@ -52,28 +45,23 @@ void* producer(void* arg){
     }
     return NULL;
 }
-// Parler dans le readme ou initialement que j'avais d'abord penser faire un while(1) if prodcount<10 : do else : break
-// sans penser qu'en fait plusieurs threads vont verifier cette condition et acceder au bloc dans le if et donc ca va produire
-// + que le seuil
 
-
-// Garder le producer's id bien comme j'ai fait, mettre un compteur de consummer et producer et compter le nombre de fois
 void* consummer(void* arg){
     static int ic = 0;
     while(1){
         sem_wait(&cremplies);
         pthread_mutex_lock(&mutex_prodcons);
-        if (consumcount>=131072 && prodcount >= 131072) //131072
+        if (consumcount>=NB_ITEMS && prodcount >= NB_ITEMS) // If nb of producted and consummed items>NB_ITEMS we stop
         {
             pthread_mutex_unlock(&mutex_prodcons);
             break;
         }
         
-        // On lit ce qu'il y a dans le buffer (ici pour l'instant on ne fait rien)
+        // Critical section
         consumcount++;
         int element = buffer[ic];
         ic = (ic+1)%N;
-        printf("Consumer: consumcount=%d, element=%d\n", consumcount, element);
+        // Debugging : printf("Consumer: consumcount=%d, element=%d\n", consumcount, element);
 
         pthread_mutex_unlock(&mutex_prodcons);
         process();
