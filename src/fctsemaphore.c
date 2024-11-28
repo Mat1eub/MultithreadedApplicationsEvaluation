eacb4c5698e01d76181eb8758d3e1bf0da0dd011
0ufti <arnaud.gheysens@student.uclouvain.be>
2024-11-28 15:42:01 +0100

    impl sem

--- src/fctsemaphore.c
+++ src/fctsemaphore.c
@@ -1,11 +1,25 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>     
+#include <unistd.h>
+
+int intitsem(int** sem){
+    *sem = malloc(sizeof(int));  // Allouer de la mémoire pour un int, pas pour un int*
+    if (!*sem) {
+        perror("malloc");
+        return 1;
+    }
+    return 0;
+}
+
+void destroysem(int* verou){
+    free(verou);
+}
 
 void lock(int* verou) {
     int etat = 1;
-    asm(
+    
+    __asm__(
         "enter:\n\t"
         "xchgl %1, %0\n\t"
         "testl %1, %1\n\t" 
@@ -13,12 +27,11 @@ void lock(int* verou) {
         : "+m"(*verou), "+r"(etat)              
         :
     );
-
 }
 
 void unlock(int* verou) {
     int etat = 0;
-    asm( 
+    __asm__( 
         "xchgl %1, %0\n\t"       
         : "+m"(*verou), "+r"(etat)           
         :                     
