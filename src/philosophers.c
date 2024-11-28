814eed80281430721868320191efca4aef29d04f
Mateo Bauvir <bauvir.mateo@gmail.com>
2024-11-28 15:54:03 +0100

    New branch to cook

--- src/philosophers.c
+++ src/philosophers.c
@@ -1,90 +1,107 @@
-#include <semaphore.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int N;
-sem_t* forks;
-#define COUNT 10e6       // Nombre de cycles penser/manger
-// For the philosphers N= nbr of philosphes but also nb of threads ?
-void* philosopher(void* param) {
-    int i = *(int*)param;             
-    int right = i;                   
-    int left = (i + 1) % N;          
+// Déclarations globales
+pthread_t* phil;               // Tableau de threads
+pthread_mutex_t* baguette;      // Tableau de mutex représentant les baguettes
+int cmpt = 0;                  // Compteur pour suivre les actions des philosophes
 
-    for (size_t j = 0; j < COUNT; j++) {
+// Fonction représentant le philosophe en train de manger
+void mange(int id) {
+    cmpt++;
+    // Simulez ici un délai si nécessaire, ex. : usleep(100);
+}
 
-        // Philosophe pense
-        // On ne fait rien car penser est instantané
+// Fonction exécutée par chaque thread
+void* philosophe(void* arg) {
+    int id = *(int*)arg;                // Récupère l'ID du philosophe
+    int left = id;                      // Baguette gauche
+    int right = (id + 1) % cmpt;        // Baguette droite (table circulaire)
 
-        if (i == N - 1) {            // Dernier philosophe prend les fourchettes dans un ordre inversé pour éviter un deadlock
-            sem_wait(&forks[right]);
-            sem_wait(&forks[left]);
+    for (int i = 0; i < 1000000; i++) { // Boucle d'actions penser/manger
+        if (left < right) {             // Ordre pour éviter un interblocage
+            pthread_mutex_lock(&baguette[left]);
+            pthread_mutex_lock(&baguette[right]);
         } else {
-            sem_wait(&forks[left]);
-            sem_wait(&forks[right]);
+            pthread_mutex_lock(&baguette[right]);
+            pthread_mutex_lock(&baguette[left]);
         }
 
-        // Philosophe mange (section critique)
-        // Ici aussi, on ne fait rien car manger est instantané.
+        mange(id); // Philosophe mange
 
-        sem_post(&forks[left]);    // Le philosophe libère les fourchettes
-        sem_post(&forks[right]);
+        pthread_mutex_unlock(&baguette[left]);  // Libère la baguette gauche
+        pthread_mutex_unlock(&baguette[right]); // Libère la baguette droite
     }
 
     return NULL;
 }
 
-int main(int argc, char const *argv[])
-{
-    char const* problem = argv[0];
-    
-    if (strcmp(problem,"./philosophers_exec")==0 || strcmp(problem,"./philo")==0){
-        if (argc != 2) {
-            fprintf(stderr, "Usage: %s N=<number_of_philosophers>\n", argv[0]);
-            return 1;
-        }
-
-        // Get N from commande line (N = nbr of philosophers = nbr of threads too)
-        N = atoi(argv[1]);
-        if (N <= 0) {
-            fprintf(stderr, "Invalid number of philosophers.\n");
-            return 1;
-        }
-
-        // Allocate dynamically the semaphore array of forks
-        forks = malloc(N*sizeof(sem_t));
-
-        if (!forks) {
-            perror("malloc");
-            return 1;
-        }
-
-        for (int i = 0; i < N; i++) {
-            sem_init(&forks[i], 0, 1);
-        }
-
-        // Threads for each philosopher
-        pthread_t philosophers[N];       
-        int indices[N];                  
-
-        // Each philosopher in a thread 
-        for (int i = 0; i < N; i++) {
-            indices[i] = i;
-            pthread_create(&philosophers[i], NULL, philosopher, &indices[i]);
-        }
-
-        // Threads' results
-        for (int i = 0; i < N; i++) {
-            pthread_join(philosophers[i], NULL);
-        }
-
-        for (int i = 0; i < N; i++) {
-            sem_destroy(&forks[i]);
-        }
-
-        free(forks);
+// Fonction principale
+int main(int argc, char* argv[]) {
+    if (argc != 2) { // Vérification des arguments
+        fprintf(stderr, "Usage: %s <nombre_de_philosophes>\n", argv[0]);
+        return 1;
     }
+
+    int PHILOSOPHES = atoi(argv[1]); // Nombre de philosophes
+    if (PHILOSOPHES <= 0) {
+        fprintf(stderr, "Le nombre de philosophes doit être un entier positif.\n");
+        return 1;
+    }
+
+    cmpt = PHILOSOPHES; // Met à jour le compteur global
+
+    // Allocation dynamique des ressources
+    phil = malloc(PHILOSOPHES * sizeof(pthread_t));
+    baguette = malloc(PHILOSOPHES * sizeof(pthread_mutex_t));
+    int* ids = malloc(PHILOSOPHES * sizeof(int));
+
+    if (!phil || !baguette || !ids) {
+        perror("Erreur d'allocation");
+        free(phil);
+        free(baguette);
+        free(ids);
+        return 1;
+    }
+
+    // Initialisation des mutex
+    for (int i = 0; i < PHILOSOPHES; i++) {
+        if (pthread_mutex_init(&baguette[i], NULL) != 0) {
+            perror("Erreur lors de l'initialisation d'un mutex");
+            free(phil);
+            free(baguette);
+            free(ids);
+            return 1;
+        }
+    }
+
+    // Création des threads
+    for (int i = 0; i < PHILOSOPHES; i++) {
+        ids[i] = i; // ID unique pour chaque philosophe
+        if (pthread_create(&phil[i], NULL, philosophe, &ids[i]) != 0) {
+            perror("Erreur lors de la création du thread");
+            free(phil);
+            free(baguette);
+            free(ids);
+            return 1;
+        }
+    }
+
+    // Attente de la fin des threads
+    for (int i = 0; i < PHILOSOPHES; i++) {
+        pthread_join(phil[i], NULL);
+    }
+
+    // Destruction des mutex
+    for (int i = 0; i < PHILOSOPHES; i++) {
+        pthread_mutex_destroy(&baguette[i]);
+    }
+
+    // Libération des ressources
+    free(phil);
+    free(baguette);
+    free(ids);
+
     return 0;
 }
