2fbe688282d68fca302587380f070e6f033de912
Mateo Bauvir <bauvir.mateo@gmail.com>
2024-11-28 09:35:49 +0100

    Almost finished spinlock
    TODO: Comments in src code and documentation in headers then check if it's working

--- src/spinlock.c
+++ src/spinlock.c
@@ -1,34 +1,45 @@
-#include <semaphore.h>
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
-#include <string.h>
 #include "../headers/spinlock.h"
 
 // Fonction init il faut malloc
-// Fonction destroy il faut free
+int spinlock_init(spinlock_t** lk){
+    *lk = malloc(sizeof(spinlock_t));
+    if(*lk==NULL){
+        return 1; // Could not allocate correctly
+    }
+    (*lk)->locked = 0; // Initialization
+    return 0; // Success
+}
 
-void spinlock_lock(spinlock_t** verou) {
+// Fonction destroy il faut free
+int spinlock_free(spinlock_t* lk){
+    if(lk == NULL){
+        return 1; // Nothing to free
+    }
+    free(lk);
+    return 0; // Success
+}
+
+void spinlock_lock(spinlock_t* lk) {
     __asm__(
         "enter:"
         "movl $1, %eax\n\t"     // Set the value of 1 in eax register (1 means locked)
-        "xchgl %eax, %0\n\t"    // Exchange atomicly de value in eax and the value from *verou
+        "xchgl %eax, %0\n\t"    // Exchange atomicly de value in eax and the value from *lk
         "testl %eax, %eax\n\t"  // if(eax==0) then ZF=0 
         "jnz enter\n\t"         // if ZF!=0 go back to enter
-        : "+m"((*verou)->locked)
+        : "+m"(lk->locked)
         :
+        :"eax"
     );
 
 }
 
-void spinlock_unlock(spinlock_t** verou) {
+void spinlock_unlock(spinlock_t* lk) {
     __asm__(
-            "movl $1, %eax\n\t"
-            "xchgl %eax,%0\n\t"
-            : "+m"((*verou)->locked)
+            "movl $0, %eax\n\t" // (\n\t helps for the inline assembly nothing to understand really)
+            "xchgl %eax,%0\n\t" // not $1 but $0 do wee need to still do an xchg or we could just do movl $0, %0 ?
+            : "+m"(lk->locked)
             : 
             );
 }
-
-// Pourquoi le \n\t ? 
-// Je metrai des commentaires sur pq on met un double pointeur
