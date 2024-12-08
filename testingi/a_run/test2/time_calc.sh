579292716968448ea84d247b0910829f80a04cf5
0ufti <arnaud.gheysens@student.uclouvain.be>
2024-12-08 15:59:30 +0100

    tout sur mon pc + ce que faut run pour 248 t

--- testingi/a_run/test2/time_calc.sh
+++ testingi/a_run/test2/time_calc.sh
@@ -8,6 +8,6 @@ for i in 2 4 8 16 32
 do
     for k in {1..5}
     do
-        /usr/bin/time -f "$1, $i, %e" -a timeout 20s ./$1.o $i  2>&1
+        /usr/bin/time -f "$1, $i, %e" -a timeout 22s ./$1.o $i  2>&1
     done
 done
