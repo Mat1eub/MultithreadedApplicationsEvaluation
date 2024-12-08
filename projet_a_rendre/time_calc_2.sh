a69dafa59a5c7c0ba714da85107afa2e391b7d2b
0ufti <arnaud.gheysens@student.uclouvain.be>
2024-12-08 17:09:11 +0100

    zip final

---
+++ projet_a_rendre/time_calc_2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+OUTPUT_FILE=$1
+
+for i in 2 4 8 16 32
+do
+    for k in {1..5}
+    do
+        /usr/bin/time -f "$2, $i, %e" -o "$OUTPUT_FILE" -a ./$2.o $((i/2)) $((i/2)) > /dev/null 2>&1
+    done
+done
