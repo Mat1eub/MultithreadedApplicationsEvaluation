ec434936f1f5be809506709ef4d78bb78a2a9b26
Arnaud Gheysens <arnaud.gheyense@student.uclouvain.be>
2024-12-08 15:32:47 +0100

    test 2 4 8

--- testingi/a_run/test1/time_calc.sh
+++ testingi/a_run/test1/time_calc.sh
@@ -4,10 +4,11 @@ OUTPUT_FILE=stderr
 
 echo "problem,nb_of_threads,time" 
 
-for i in 2 4 8 16 32
+for i in 2 4 8
 do
     for k in {1..5}
     do
-        /usr/bin/time -f "$1, $i, %e" -a timeout 20s ./$1.o $i  2>&1
+        /usr/bin/time -f "$1, $i, %e" -a timeout 22s ./$1.o $i  2>&1
+        /usr/bin/time -f "$2, $i, %e" -a timeout 22s ./$2.o $i  2>&1
     done
 done
