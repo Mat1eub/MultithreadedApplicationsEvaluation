df1f8e82609cde2e5a2346d7a028d76924da0e7c
Mateo Bauvir <bauvir.mateo@gmail.com>
2024-12-13 13:28:40 +0100

    Structure and makefile upgrade
    Almost finished project

---
+++ philosophers_time copy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+programs=("philo" "philots" "philotts")
+threads=(2 4 8 16 32)
+output="results/performances_philo.csv"
+
+echo "problem,nb_of_threads,time" > $output # Headers of columns
+
+for prob in "${programs[@]}"; do
+    # Ensure the program is compiled
+    if ! make $prob; then
+        echo "Error: Could not compile $prob" #>&2
+        continue
+    fi
+
+    # Loop through thread configurations
+    for t in "${threads[@]}"; do
+        for _ in {1..5}; do
+            exec_time=$(/usr/bin/time -f "%e" ./$prob $t 2>&1 > /dev/null )
+            echo "$prob,$t,$exec_time" >> $output
+        done
+    done
+done
--- prodcons_time copy.sh
+++ prodcons_time copy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-export LC_NUMERIC="C"
 
-programs=("philo" "prodcons" "readwrite_graphs")
+programs=("philo" "philo_tas" "philo_tts")
 threads=(2 4 8 16 32)
-output="results/performances_part1.csv"
+output="results/performances_prodcons.csv"
 
 echo "problem,nb_of_threads,time" > $output # Headers of columns
 
@@ -17,8 +16,7 @@ for prob in "${programs[@]}"; do
     # Loop through thread configurations
     for t in "${threads[@]}"; do
         for _ in {1..5}; do
-            exec_time=$(/usr/bin/time -p ./$prob $t 2>&1 | grep real | awk '{print $2}')
-            printf -v exec_time "%.3f" "$exec_time"
+            exec_time=$(/usr/bin/time -f "%e" ./$prob $t 2>&1 > /dev/null )
             echo "$prob,$t,$exec_time" >> $output
         done
     done
